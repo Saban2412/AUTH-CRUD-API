f9d1ba3614f49681eecbe1792841217fe6b4a9e3
Saban2412 <emir.sabanovic.23@size.ba>
2026-08-19 13:05:29 +0200

    PRESTAGE: swagger.json update

--- app.js
+++ app.js
@@ -64,10 +64,24 @@ app.get("/books", async (req, res) => {
         }
         return res.json(results.rows);
     } catch (error) {
-        console.error('Error fetching books: ', error.message);
-        res.status(500).json({ error: "Greška na serveru pri dobavljanju knjiga." });
+        console.error('Error fetching data: ', error.message);
+        res.status(500).json({ error: "Internal server error." });
     }
 });
+
+app.get("/books/:id", async(req,res)=>{
+    const reqId = req.params.id;
+    try{
+        const result = await pool.query("SELECT * FROM books WHERE id = $1", [reqId]);
+        if(result.rows.length===0){
+            return res.status(404).json(`Boook with id: ${reqId} is not found!`);
+        }
+        return res.status(200).json(result.rows[0]);
+    }catch(error){
+        console.error('Error fetching data: ', error.message);
+        res.status(500).json({ error: "Internal server error." });
+    }
+})
 
 //----------------
 //  DELAY & START
